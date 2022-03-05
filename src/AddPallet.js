c575aacd1fb559047c6d402133c99a9333de0bfa
KaliKru <89308408+KaliKru@users.noreply.github.com>
2022-03-06 00:43:58 +0100

    AddPallet color changed by user input

--- src/AddPallet.js
+++ src/AddPallet.js
@@ -1,6 +1,70 @@
 import React, {useState} from 'react';
 import './addPallet.css';
 
+
+function AddPallet() {
+    const [pallet, setPallet] = useState([""]);
+    const [color, setColor] = useState([]);
+    const [rgb, setRgb] = useState([]);
+
+
+    const addColor = () => {
+        setPallet(prev => [...prev, color])
+        console.log(color)
+    }
+
+    const handleColor = (event) => {
+        setColor(event.target.value)
+    }
+
+
+    const handleRGB = (rgb) => {
+        setRgb(rgb)
+    }
+
+
+    const inputColor = (color) => {
+        setColor(color);
+    }
+
+
+    return (
+        <div>
+            <div className={"stylePallet"}>
+                <div className={"styleRatio"}>Your pallet</div>
+                <button onClick={addColor}>new color</button>
+                {pallet.map(color => <div style={{
+                        width: 50,
+                        height: 50,
+                        backgroundColor: color,
+                    }} onChange={handleColor} onInput={() => inputColor('blue')}>{color}</div>
+                )}
+                <div>{color}</div>
+
+                <form>
+                    <input type={"text"} value={color} placeholder="type color" onChange={handleColor}/>
+                    <div style={{backgroundColor: "lightgray"}}>
+                        <input placeholder="type RGB number" onChange={handleRGB} onSubmit={() => handleRGB('rgb')}/>
+
+                        <h5 >{isNaN(color) ? "num only" : "HEX:"}<input onChange={handleColor}/></h5>
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    );
+}
+
+export default AddPallet;
+
+
+// rgb
+// <input placeholder="type RGB number" onChange={handleRGB} onSubmit={() => handleRGB('rgb')}/>
+
+/*
+
+//wersja z 5.03. ale tego nie chce
 function AddPallet({onAddPallet}) {
     const [pallet, setPallet] = useState("");
     const [color, setColor] = useState("");
@@ -17,6 +81,8 @@ function AddPallet({onAddPallet}) {
     };
 
     let addColor;
+
+
 
 
     return (
@@ -40,6 +106,8 @@ function AddPallet({onAddPallet}) {
                     </div>
                     <button onClick={addColor} icon={"fas fa-plus-circle"}>add color</button>
 
+
+
                 </form>
 
             </div>
@@ -51,6 +119,8 @@ function AddPallet({onAddPallet}) {
 export default AddPallet;
 
 
+
+ */
 
 
 /* //versja z 4.03.
